f8764f988f3f57f6b0ddf50adcb747b861d8bd8f
Bi9Snail <150133976+Bi9Snail@users.noreply.github.com>
2023-11-12 16:24:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+# from dotenv import load_dotenv
+# load_dotenv()
+from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain.chains import RetrievalQA
 import streamlit as st
-import tempfile
 import os
+import tempfile
 
-#제목
-st.title("ChatPDF")
+
+st.title("ChatPDF! 섹스보다 재밌다!")
 st.write("---")
-
-#파일 업로드
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("파일 업로드", type=['pdf'])
 st.write("---")
 
 def pdf_to_document(uploaded_file):
@@ -30,33 +30,44 @@ def pdf_to_document(uploaded_file):
     pages = loader.load_and_split()
     return pages
 
-#업로드 되면 동작하는 코드
+
+#업로드 되면 동장하는 코드
 if uploaded_file is not None:
     pages = pdf_to_document(uploaded_file)
 
-    #Split
-    text_splitter = RecursiveCharacterTextSplitter(
-        # Set a really small chunk size, just to show.
+
+
+
+    #스플리터
+    text_splitter = CharacterTextSplitter(
+        separator = "\n\n",
         chunk_size = 300,
         chunk_overlap  = 20,
         length_function = len,
-        is_separator_regex = False,
+        is_separator_regex = False
     )
+
     texts = text_splitter.split_documents(pages)
 
-    #Embedding
+    #임베드
     embeddings_model = OpenAIEmbeddings()
 
-    # load it into Chroma
+    #크로마로 불러오기
     db = Chroma.from_documents(texts, embeddings_model)
 
-    #Question
-    st.header("PDF에게 질문해보세요!!")
-    question = st.text_input('질문을 입력하세요')
+    #헤더
+    st.header("PDF를 기반으로 질문해주세요")
 
-    if st.button('질문하기'):
-        with st.spinner('Wait for it...'):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
-            result = qa_chain({"query": question})
-            st.write(result["result"])
+    #질문 입력
+    question = st.text_input('질문을 입력해주세요')
+    
+    #버튼 생성
+    if st.button('시작'):
+
+        #질문
+        llm = ChatOpenAI(model_name = "gpt-3.5-turbo-16k", temperature=0)
+        qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
+        result = qa_chain({"query": question})
+
+        st.write(result)
+
